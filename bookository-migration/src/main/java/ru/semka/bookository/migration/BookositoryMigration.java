a689e7c6d278ee43bd66de176d8e2536c525b6cc
Andrey Semka <k9nz00@yandex.ru>
2023-07-16 18:20:25 +0300

    SPBR-2: Implemented simple migration module

--- bookository-migration/src/main/java/ru/semka/bookository/migration/BookositoryMigration.java
+++ bookository-migration/src/main/java/ru/semka/bookository/migration/BookositoryMigration.java
@@ -1,13 +1,18 @@
 package ru.semka.bookository.migration;
 
-/**
- * Hello world!
- *
- */
-public class BookositoryMigration
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan("ru.semka.bookository.migration.configuration")
+@Log
+public class BookositoryMigration {
+    public static void main(String[] args) {
+        log.info("Starting migration service");
+        SpringApplication.run(BookositoryMigration.class, args);
     }
 }
