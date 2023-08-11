748af27e78316ded8a2016ca01ccda0045f0670c
natalia_semka <seraya.93@inbox.ru>
2023-08-11 23:53:02 +0300

    wip: bookCard layout

--- bookository-web/app/src/components/BookCardView.vue
+++ bookository-web/app/src/components/BookCardView.vue
@@ -1,87 +1,73 @@
 <template>
-  <div class="card-view">
-    <div class="body">
-      <div v-if="!book">Нет такой книги</div>
+  <div class="card-view p-10 flex items-center justify-center">
+    <div v-if="loading">Загружается...</div>
 
-      <template v-else>
-        <div class="flex gap-2 w-full">
-          <div class="cover"/>
+    <div v-else-if="!loading && !book">Нет такой книги</div>
 
-          <div class="grow space-y-2">
-            <div class="input-label">
-              <label for="name">Название</label>
-              <input
-                id="name"
-                type="text"
-                class="name"
-                :value="book.name"
-              >
-            </div>
+    <form
+      v-else
+      action=""
+      class="card-grid"
+    >
+      <img
+        class="cover"
+        :src="cover"
+        alt=""
+      >
 
-            <div class="input-label">
-              <label for="author">Автор</label>
-              <input
-                id="author"
-                type="text"
-                class="name"
-                :value="book.author"
-              >
-            </div>
+      <div class="app-input-label">
+        <label for="name">Название</label>
+        <input
+          id="name"
+          type="text"
+          class="app-input"
+          :value="book.name"
+        >
+      </div>
 
-            <div class="input-label">
-              <label for="genre">Жанр</label>
-              <input
-                id="genre"
-                type="text"
-                class="name"
-                :value="book.genre"
-              >
-            </div>
+      <div class="app-input-label">
+        <label for="author">Автор</label>
+        <input
+          id="author"
+          type="text"
+          class="app-input"
+          :value="book.author"
+        >
+      </div>
 
-            <div class="input-label">
-              <label for="category">Категория</label>
-              <input
-                id="category"
-                type="text"
-                class="name"
-                :value="book.category"
-              >
-            </div>
+      <div class="app-input-label">
+        <label for="genre">Жанр</label>
+        <input
+          id="genre"
+          type="text"
+          class="app-input"
+          :value="book.genre"
+        >
+      </div>
 
-            <div class="input-label">
-              <label for="language">Язык оригинала</label>
-              <select
-                id="language"
-                name="language"
-                class="app-select"
-                :value="book.language"
-              >
-                <option
-                  v-for="language in languages"
-                  :key="language.code"
-                  :value="language.code"
-                  :selected="language.code === book.language"
-                >
-                  {{ language.name }}
-                </option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <textarea
-          class="description"
-          :value="book.annotation"
+      <div class="app-input-label">
+        <label for="category">Категории</label>
+        <AppSelect
+          class="app-input"
+          :options="categories"
+          :selected="selectedCategory"
         />
-      </template>
-    </div>
+      </div>
+
+      <textarea
+        class="app-input annotation leading-normal"
+        :value="book.annotation"
+      />
+    </form>
   </div>
 </template>
 
 <script setup>
 import defaultBoard from '../default-board.js'
-import { computed } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+
+import AppSelect from './AppSelect.vue'
 
 const languages = [{
   name: 'Русский',
@@ -106,56 +92,81 @@ const BOOK_MODEL = {
 }
 
 const route = useRoute()
-
-const shelfId = route.params.shelfId
 const bookId = route.params.bookId
 
-const book = computed(() => {
-  const shelf = defaultBoard.shelves.find(item => item.id === shelfId)
+const loading = ref(false)
+const book = ref(null)
+const getBook = () => {
+  return fetch('http://192.168.1.110:8020/api/books/1')
+    .then((response) => response.json())
+    .then((response) => (book.value = response || null))
+}
 
-  return shelf?.cards.find(book => book.id === bookId) || null
+const selectedCategory = ref('')
+const categories = ref([])
+const getCategories = () => {
+  return fetch('http://192.168.1.110:8020/api/book-categories')
+    .then((response) => response.json())
+    .then((response) => (categories.value = response))
+}
+
+const cover = computed(() => {
+  return `data:image/gpeg;base64,${book.value.bigPreview}` || ''
+})
+
+onMounted(() => {
+  // Promise.all([getBook(), getCategories()])
+  //   .catch((error) => {
+  //     console.log(error)
+  //   }).finally(() => {
+  //     loading.value = false
+  //   })
+
+  book.value = defaultBoard.shelves[0].cards[0]
+
+  categories.value = [
+    {
+      name: 'Русская литература',
+      id: '1'
+    },
+    {
+      name: 'Зарубежная литература',
+      id: '2'
+    },
+  ]
 })
 </script>
 
 <style>
-.card-view {
-  @apply relative bg-white p-10;
-}
-
-.card-view .body {
-  @apply flex flex-col flex-grow items-start justify-between;
+.card-grid {
+  @apply grid gap-x-5 gap-y-4;
+  grid-template-rows: repeat(10, 1fr);;
+  grid-template-columns: 1fr 2fr;
 }
 
 .card-view .cover {
-  @apply border-2 border-blue-300 rounded-md;
-  min-width: 200px;
+  @apply border border-blue-300 rounded-md;
   background: url('../assets/vue.svg') no-repeat center;
   background-size: 50%;
+  grid-row-start: 1;
+  grid-row-end: 5;
 }
 
-.card-view .name {
-  @apply p-2 w-full mr-2 block text-xl font-bold;
-  @apply border-2 border-blue-300 rounded-md;
+.app-input-label {
+  @apply grid grid-flow-col items-center gap-4;
+  grid-template-columns: 1fr 5fr;
+  @apply text-sm font-bold;
+  text-align: left;
 }
 
-.card-view .description {
-  @apply relative w-full bg-transparent px-2 ;
-  @apply mt-2 h-64 leading-normal;
-  @apply border-2 border-blue-300 rounded-md;
+.app-input {
+  @apply p-2 font-normal;
+  @apply border border-blue-300 rounded-md;
 }
 
-.app-select {
-  @apply p-2 w-full;
-  @apply border-2 border-blue-300 rounded-md;
-}
-
-.input-label {
-  @apply flex items-center;
-}
-
-.input-label label{
-  width: 150px;
-  margin-right: 20px;
-  text-align: end;
+.annotation {
+  grid-row-start: 5;
+  grid-row-end: 9;
+  grid-column: span 2 / span 2;
 }
 </style>
