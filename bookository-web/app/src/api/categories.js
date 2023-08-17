ff7120d6e71befe841e30218889c80f79962070c
natalia_semka <seraya.93@inbox.ru>
2023-08-17 21:56:59 +0300

    Add Categories API

--- bookository-web/app/src/api/categories.js
+++ bookository-web/app/src/api/categories.js
@@ -11,7 +11,51 @@ import { BASE_URL } from '../constants.js'
  */
 export default {
   getCategories() {
-    return fetch(`${BASE_URL}/book-categories`)
+    return fetch(`${ BASE_URL }/book-categories`)
+      .then((response) => response.json())
+      .then((response) => response)
+  },
+
+  /**
+   * @param {Category} category
+   * @returns {Promise<Category>}
+   */
+  createCategory(category) {
+    const options = {
+      method: 'POST',
+      body: category
+    }
+
+    return fetch(`${ BASE_URL }/book-categories`, options)
+      .then((response) => response.json())
+      .then((response) => response)
+  },
+
+  /**
+   * @param {Category} category
+   * @returns {Promise<void>}
+   */
+  updateCategory(category) {
+    const options = {
+      method: 'PUT',
+      body: category
+    }
+
+    return fetch(`${ BASE_URL }/book-categories/${ category.id }`, options)
+      .then((response) => response.json())
+      .then((response) => response)
+  },
+
+  /**
+   * @param {number} id
+   * @returns {Promise<void>}
+   */
+  deleteCategory(id) {
+    const options = {
+      method: 'DELETE'
+    }
+
+    return fetch(`${ BASE_URL }/book-categories/${ id }`, options)
       .then((response) => response.json())
       .then((response) => response)
   }
