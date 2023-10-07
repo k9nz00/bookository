0baf306c9d212f958f1cdaf4c8a4cfcc2d1d3fc7
natalia_semka <seraya.93@inbox.ru>
2023-10-07 20:39:50 +0300

    Adds UI components

---
+++ bookository-web/app/src/components/AppButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    type="button"
+    class="app-button"
+    @click="click"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+const emit = defineEmits(['click'])
+
+const click = () => {
+  emit('click')
+}
+</script>
