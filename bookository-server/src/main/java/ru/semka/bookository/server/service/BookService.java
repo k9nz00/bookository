5e8098bc0a2172f685b08b6a11c2c807e0afce78
Andrey Semka <k9nz00@yandex.ru>
2023-10-25 23:10:17 +0300

    SPBR-30: Added support for filtration by createdAt

--- bookository-server/src/main/java/ru/semka/bookository/server/service/BookService.java
+++ bookository-server/src/main/java/ru/semka/bookository/server/service/BookService.java
@@ -2,10 +2,10 @@ package ru.semka.bookository.server.service;
 
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import ru.semka.bookository.server.rest.dto.book.BookCriteriaDto;
 import ru.semka.bookository.server.rest.dto.book.BookDetailsUiDto;
 import ru.semka.bookository.server.rest.dto.book.BookRequestDto;
 import ru.semka.bookository.server.rest.dto.book.BookUiDto;
+import ru.semka.bookository.server.rest.dto.book.BooksCriteriaDto;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -22,7 +22,7 @@ public interface BookService {
 
     void attachBook(int bookId, MultipartFile book) throws IOException;
 
-    Collection<BookUiDto> getBooks(BookCriteriaDto criteriaDto);
+    Collection<BookUiDto> getBooks(BooksCriteriaDto criteriaDto);
 
     BookDetailsUiDto getDetails(int bookId);
 
