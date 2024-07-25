6ec0b5ac10fbe2b710c4c511d1af87fd7d625715
Andrey Semka <k9nz00@yandex.ru>
2024-07-25 18:29:56 +0300

    hotfix: Изменение структуры ответа при получении данных обложки

--- bookository-server/src/main/java/ru/semka/bookository/server/rest/dto/cover/CoverDto.java
+++ bookository-server/src/main/java/ru/semka/bookository/server/rest/dto/cover/CoverDto.java
@@ -1,9 +1,4 @@
 package ru.semka.bookository.server.rest.dto.cover;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
 public record CoverDto(String imageData) {
 }
