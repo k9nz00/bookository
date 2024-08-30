367906ac9eb15f38f59763dc6b9c435a74e6561f
Andrey Semka <k9nz00@yandex.ru>
2024-08-31 00:02:43 +0300

    feature/SPBR-34: Переделка модуля миграции

--- bookository-env/dockerfiles/postgres/postgres-migration-1.sh
+++
@@ -1,3 +0,0 @@
-pg_ctl -D "$PGDATA"	-w stop
-pg_ctl -D "$PGDATA" -o "-c listen_addresses='0.0.0.0'" -w start
-java -DdriverClassName=org.postgresql.Driver -DjdbcUrl=jdbc:postgresql://0.0.0.0:5432/bookository -Ddatabase=$POSTGRES_DB -Dusername=$POSTGRES_USER -Dpassword=$POSTGRES_PASSWORD -jar /opt/migration.jar
