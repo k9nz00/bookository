5500ad4cd25c2f9411b579964a589346daa43cdb
natalia_semka <seraya.93@inbox.ru>
2024-09-07 17:22:36 +0300

    Removed unused components

--- bookository-web/app/src/router.js
+++ bookository-web/app/src/router.js
@@ -1,12 +1,10 @@
 import MainView from './views/MainView.vue'
 import BookCardView from './components/BookDetails.vue'
-import AddBookCard from './components/AddBookCard.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: MainView },
   { path: '/books/:bookId', component: BookCardView },
-  { path: '/books/:shelfId', component: AddBookCard },
 ]
 
 export const router = createRouter({
